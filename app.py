fda589b1a9344484035aa04c15ee029dc1422557
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2021-02-01 10:35:20 +0200

    add polling

--- app.py
+++ app.py
@@ -11,3 +11,7 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.reply(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
