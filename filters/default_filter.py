68e248f6c989f3c95a7939987c4f7cd3e06dd08d
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2021-02-05 13:41:24 +0200

    Added filters

---
+++ filters/default_filter.py
@@ -0,0 +1,11 @@
+def command_start():
+    string = 'Hello!, this is Bot for Accounting Finance\n' \
+             'show command /help'
+    return string
+
+def command_help():
+    string = '/categories - Show Categories\n' \
+             '/expenses_month - Show expenses month\n' \
+             '/expenses_week -- Expenses on week\n' \
+             '/expenses_day -- Expenses today'
+    return string
